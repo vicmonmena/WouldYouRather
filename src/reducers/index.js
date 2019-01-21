a1aa8d7d1593a12d47b885398e02b9154b6e78be
vmontanomena <vicente.montanomena@telefonica.com>
2019-01-21 22:38:30 +0100

    refactor:removing final semicolons

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 import { loadingBarReducer } from 'react-redux-loading'
-import questions from './questions';
-import users from './users';
-import authedUser from './authedUser';
+import questions from './questions'
+import users from './users'
+import authedUser from './authedUser'
 export default combineReducers({
   authedUser,
   users,
   questions,
   loadingBar: loadingBarReducer
-});
+})
