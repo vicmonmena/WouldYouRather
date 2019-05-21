2a47fdd5c75e680c60e21895bf92de6623ecceca
vmontanomena <vicente.montanomena@telefonica.com>
2019-05-21 18:04:33 +0200

    Adding crypress for testing

--- cypress/integration/react-redux/login.spec.js
+++ cypress/integration/react-redux/login.spec.js
@@ -2,12 +2,9 @@ describe('Log In', () => {
   it('succesfully performs login action', () => {
     // visit 'baseUrl'
     cy.visit('/');
-    cy.get('select').select('user-1')
+    cy.get('select').select('Sarah Edo')
     cy.get('form').submit()
-    // assert if we are in good place - search for a 'smarter world phrase
-    cy.contains('smarter world');
-
     // verify that we were redirected
-    cy.url({ timeout: 3000 }).should('includes', '/home/');
+    cy.url({ timeout: 3000 }).should('includes', 'home');
   });
 });
